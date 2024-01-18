646628739e85e2fc6598b16de198c2ef743ff759
Sloan Gauthier <sloangauthier@gmail.com>
2024-01-18 13:00:09 +0100

    2nd part

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ import { readFileSync } from "fs";
 const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
-console.log({ test, final });
+const part1 = (data) => {
+  const fishes = test.split(",");
+  return fishes;
+  //   console.log(fishes);
+};
+console.log(part1(test));
