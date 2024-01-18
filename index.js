cc3bfed6afcd95eb9df63f2b344ddeca4b77193b
Sloan Gauthier <sloangauthier@gmail.com>
2024-01-18 19:29:04 +0100

    part3

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
 const part1 = (data) => {
-  const fishes = test.split(",");
+  const years = 80;
+  const fishes = final.split(",").map(Number);
+
+  for (let year = 0; year < years; year++) {
+    const saveLength = fishes.length;
+    for (let i = 0; i < saveLength; i++) {
+      if (fishes[i] === 0) {
+        fishes[i] = 6;
+        fishes.push(8);
+      } else {
+        fishes[i]--;
+      }
+    }
+  }
   return fishes;
-  //   console.log(fishes);
 };
-console.log(part1(test));
+
+const result = part1(test);
+console.log(result.length);
